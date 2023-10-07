28131d3e0fd08cc47574468721583cfc0871d24e
Dede Ariansya <me@dedeard.my.id>
2023-10-08 01:01:40 +0800

    🐞 fix: empty response.

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,11 +1,12 @@
 import { AuthGuard } from '@nestjs/passport';
-import { Controller, Post, Put, Req, Body, UseGuards, UseInterceptors, UploadedFile, Get } from '@nestjs/common';
+import { Controller, Post, Put, Req, Body, UseGuards, UseInterceptors, UploadedFile, Get, Res, HttpStatus } from '@nestjs/common';
 import { UserRecord } from 'firebase-admin/lib/auth/user-record';
 import { ProfilesService } from './profiles.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ValidateImagePipe } from '../common/pipes/validate-image.pipe';
+import { Response } from 'express';
 
 @UseGuards(AuthGuard('cookie-or-bearer'))
 @Controller('profiles')
@@ -13,8 +14,13 @@ export class ProfilesController {
   constructor(private readonly profilesService: ProfilesService) {}
 
   @Get()
-  findOne(@Req() { user }: { user: UserRecord }) {
-    return this.profilesService.findOne(user.uid);
+  async findOne(@Res() res: Response, @Req() { user }: { user: UserRecord }) {
+    const profile = await this.profilesService.findOne(user.uid);
+    if (!profile) {
+      res.status(HttpStatus.NOT_FOUND).json({ message: 'Profile not found' });
+    } else {
+      res.json(profile);
+    }
   }
 
   @Post()
