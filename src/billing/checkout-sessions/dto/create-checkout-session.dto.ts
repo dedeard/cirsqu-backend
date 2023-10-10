8c795c8b5f149c5fd5c6dbcc56f9af93b8f84d44
Dede Ariansya <me@dedeard.my.id>
2023-10-11 00:08:42 +0800

    🐞 fix: priceId is required

--- src/billing/checkout-sessions/dto/create-checkout-session.dto.ts
+++ src/billing/checkout-sessions/dto/create-checkout-session.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from 'class-validator';
+import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateCheckoutSessionDto {
   @IsString()
+  @IsNotEmpty()
   priceId: string;
 }
