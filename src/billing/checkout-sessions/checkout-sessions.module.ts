1cf3d3b71e7d3d55edb018faca7433ed5579257a
Dede Ariansya <me@dedeard.my.id>
2023-10-11 19:57:19 +0800

    🐞 fix: import common module.

--- src/billing/checkout-sessions/checkout-sessions.module.ts
+++ src/billing/checkout-sessions/checkout-sessions.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CheckoutSessionsService } from './checkout-sessions.service';
 import { CheckoutSessionsController } from './checkout-sessions.controller';
+import { CommonModule } from '../../common/common.module';
 
 @Module({
+  imports: [CommonModule],
   controllers: [CheckoutSessionsController],
   providers: [CheckoutSessionsService],
 })
