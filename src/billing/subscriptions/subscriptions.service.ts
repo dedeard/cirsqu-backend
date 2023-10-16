e44983b66afeca3989a7c32c7673bfc620f9f26f
Dede Ariansya <me@dedeard.my.id>
2023-10-16 16:47:44 +0800

    ⏪ rollback: make the code clean.

--- src/billing/subscriptions/subscriptions.service.ts
+++ src/billing/subscriptions/subscriptions.service.ts
@@ -1,4 +1,3 @@
-import type { Stripe } from 'stripe';
 import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { StripeService } from '../../common/services/stripe.service';
 
@@ -13,39 +12,18 @@ export class SubscriptionsService {
     const { subscription } = user.profile;
 
     if (subscription.lifetime?.paymentIntentStatus === 'succeeded') {
-      return this.getLifetimePaymentIntent(subscription);
+      return this.stripe.paymentIntents.retrieve(subscription.lifetime.paymentIntentId, {
+        expand: ['latest_charge'],
+      });
     }
 
-    if (subscription.recurring?.subscriptionStatus === 'active') {
-      return this.getRecurringSubscription(subscription);
+    if (subscription.recurring?.subscriptionStatus) {
+      return this.stripe.subscriptions.retrieve(subscription.recurring.subscriptionId, {
+        expand: ['latest_invoice', 'next_pending_invoice_item_invoice'],
+      });
     }
 
     throw new NotFoundException('No active subscriptions found');
-  }
-
-  // Get payment intent for lifetime subscription
-  private async getLifetimePaymentIntent(subscription: any) {
-    const paymentIntent = await this.stripe.paymentIntents.retrieve(subscription.lifetime.paymentIntentId);
-
-    if (paymentIntent.latest_charge) {
-      paymentIntent.latest_charge = await this.stripe.charges.retrieve(paymentIntent.latest_charge.toString());
-    }
-
-    return paymentIntent;
-  }
-
-  // Get recurring subscription with latest invoices
-  private async getRecurringSubscription(subscription: any) {
-    const subs: Stripe.Subscription & { latest_invoices?: any } = await this.stripe.subscriptions.retrieve(
-      subscription.recurring.subscriptionId,
-    );
-
-    subs.latest_invoices = await this.stripe.invoices.list({
-      subscription: subs.id,
-      expand: ['data.charge'],
-    });
-
-    return subs;
   }
 
   // Retrieve active subscriptions for a given user
