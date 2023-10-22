3d35b14750d081bf8e564b68def9414008f52323
Dede Ariansya <me@dedeard.my.id>
2023-10-22 22:45:52 +0800

    ✨feature: create  subjects index and replace lessons index id using slug.

--- seeders/contents/algolia.ts
+++ seeders/contents/algolia.ts
@@ -8,7 +8,21 @@ const initAlgolia = async () => {
   const lessons = [];
 
   for (const subjectId in contents.subjects) {
-    subjects.push({ objectID: subjectId, ...contents.subjects[subjectId] });
+    const subject = contents.subjects[subjectId];
+    const lessons: string[] = [];
+
+    for (const lessonId in contents.lessons) {
+      if (contents.lessons[lessonId].subjects.includes(subjectId)) {
+        lessons.push(lessonId);
+      }
+    }
+
+    subjects.push({
+      subjectId,
+      objectID: subject.slug,
+      ...subject,
+      lessonCount: lessons.length,
+    });
   }
 
   for (const lessonId in contents.lessons) {
@@ -30,18 +44,32 @@ const initAlgolia = async () => {
     }
 
     lessons.push({
-      objectID: lessonId,
+      lessonId,
+      objectID: lesson.slug,
       title: lesson.title,
       slug: lesson.slug,
       description: lesson.description,
       seconds: episodes.reduce((total, episode) => total + episode.seconds, 0),
       subjects: lesson.subjects.map((id) => {
-        const subject = subjects.find((el) => el.objectID === id);
+        const subject = subjects.find((el) => el.subjectId === id);
         return { slug: subject.slug, name: subject.name };
       }),
       createdAt: lesson.createdAt._seconds,
       episodes: episodes,
     });
+  }
+
+  try {
+    await client.initIndex('subjects').delete();
+
+    const index = client.initIndex('subjects');
+    await index.setSettings({
+      customRanking: ['desc(lessonCount)'],
+    });
+    await index.saveObjects(subjects);
+    console.log('Updated Algolia subjects index');
+  } catch (error: any) {
+    console.error(`Failed to update subjects Algolia index`, error.message);
   }
 
   try {
@@ -53,9 +81,9 @@ const initAlgolia = async () => {
       attributesForFaceting: ['searchable(subjects.slug)'],
     });
     await index.saveObjects(lessons);
-    console.log('Updated Algolia index');
+    console.log('Updated Algolia lessons index');
   } catch (error: any) {
-    console.error(`Failed to update Algolia index`, error.message);
+    console.error(`Failed to update Algolia lessons index`, error.message);
   }
 };
 
