10d9950004b55773bbf875e8521972acdfe49c18
Dede Ariansya <me@dedeard.my.id>
2023-10-23 23:51:50 +0800

    ✨feature: add timestamps.

--- src/comments/comments.repository.ts
+++ src/comments/comments.repository.ts
@@ -1,10 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { CollectionReference, DocumentData } from 'firebase-admin/firestore';
+import { CollectionReference, DocumentData, FieldValue } from 'firebase-admin/firestore';
 import { AdminService } from '../common/services/admin.service';
 
 @Injectable()
 export class CommentsRepository {
   public readonly collection: CollectionReference<DocumentData>;
+  private readonly serverTimestamp = FieldValue.serverTimestamp;
 
   constructor(private readonly admin: AdminService) {
     this.collection = this.admin.db.collection('comments');
@@ -28,12 +29,12 @@ export class CommentsRepository {
   }
 
   create(data: IComment) {
-    return this.collection.add(data);
+    return this.collection.add({ data, createdAt: this.serverTimestamp() });
   }
 
   update(commentId: string, data: { body?: string; likes?: string[] }, promise?: () => Promise<void>) {
     return this.admin.db.runTransaction(async (t) => {
-      t.update(this.collection.doc(commentId), data);
+      t.update(this.collection.doc(commentId), { ...data, updatedAt: this.serverTimestamp() });
       await promise?.();
     });
   }
