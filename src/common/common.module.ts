9a280d44c8040680648b59eefbb47476f71076b1
Dede Ariansya <me@dedeard.my.id>
2023-10-30 00:37:12 +0800

    🐞 fix: notification service not found.

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -3,9 +3,10 @@ import { AdminService } from './services/admin.service';
 import { StorageService } from './services/storage.service';
 import { StripeService } from './services/stripe.service';
 import { AlgoliaService } from './services/algolia.service';
+import { NotificationsService } from './services/notifications.service';
 
 @Module({
-  exports: [AdminService, StorageService, StripeService, AlgoliaService],
-  providers: [AdminService, StorageService, StripeService, AlgoliaService],
+  exports: [AdminService, StorageService, StripeService, AlgoliaService, NotificationsService],
+  providers: [AdminService, StorageService, StripeService, AlgoliaService, NotificationsService],
 })
 export class CommonModule {}
